200cd3f8c65776399063e97c3205d3e6676b41df
gokuleshvar <163369258+gokuleshvarkirubhakar@users.noreply.github.com>
2024-07-19 17:38:09 +0530

    Create color_code_converter.h

---
+++ color_code_converter.h
@@ -0,0 +1,23 @@
+#ifndef COLOR_CODE_CONVERTER_H_
+#define COLOR_CODE_CONVERTER_H_
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+
+extern const int MAX_COLORPAIR_NAME_CHARS;
+extern int numberOfMajorColors;
+extern int numberOfMinorColors;
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+
+#endif // COLOR_CODE_CONVERTER_H_
