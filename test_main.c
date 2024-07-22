2e49a60186c55e4c59c703bdf6d4be2b5ce1746f
gokuleshvar <163369258+gokuleshvarkirubhakar@users.noreply.github.com>
2024-07-22 10:06:40 +0530

    Create test_main.c

---
+++ test_main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <test_color_pair_num_mapping.h>
+#include <print_color_coding_manual.h>
+
+int main() {
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+    
+    printf("\nColor Code Manual:\n");
+    PrintColorCodeManual();
+    
+    return 0;
+}
