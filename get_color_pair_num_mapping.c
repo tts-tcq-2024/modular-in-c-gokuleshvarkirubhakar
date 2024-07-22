7cf3ce1de442cc53156b493f388ac3122a1f5836
gokuleshvar <163369258+gokuleshvarkirubhakar@users.noreply.github.com>
2024-07-22 10:07:36 +0530

    Update get_color_pair_num_mapping.c

--- get_color_pair_num_mapping.c
+++ get_color_pair_num_mapping.c
@@ -29,18 +29,3 @@ ColorPair GetColorFromPairNumber(int pairNumber) {
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
 }
-
-void PrintColorCodeManual(void) {
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    int pairNumber = 0;
-    ColorPair colorPair;
-    
-    for (int major = 0; major < numberOfMajorColors; major++) {
-        for (int minor = 0; minor < numberOfMinorColors; minor++) {
-            colorPair = (ColorPair){major, minor};
-            pairNumber = GetPairNumberFromColor(&colorPair);       
-            ColorPairToString(&colorPair, colorPairNames);
-            printf("%d - %s\n", pairNumber, colorPairNames);
-        }
-    }
-}
